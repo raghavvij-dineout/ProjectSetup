21724f83052a531dd6a7b8bb4df4741a14435e60
raghavvij <raghav.vij@dineout.co.in>
2019-02-19 12:14:02 +0530

    [#Developer] Added the basic setup

---
+++ ProjectSetup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectSetup
+//
+//  Created by Raghav Vij on 19/02/19.
+//  Copyright © 2019 Raghav Vij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
